f7fff80abcd33b5ce25c15fe7e2bb9fc81668aeb
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-04-04 07:35:47 +0000

    Add project files.

---
+++ welcome_massage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Media;
+using System.IO;
+namespace voice_greeting
+{
+    public class welcome_massage
+    {
+        public welcome_massage()
+        {
+            //INSIDE CONSTRUCTOR
+            //getting the full location for the project
+            string full_location = AppDomain.CurrentDomain.BaseDirectory;
+            // replace the bin\Debug folder in the full_location
+            string new_path = full_location.Replace("bin\\Debug\\", "");
+
+            //try and catch
+            try
+            {
+                string full_path = Path.Combine(new_path,"greeting.wav");
+                //creat an instance to play sound
+                using (SoundPlayer play = new SoundPlayer(full_path))
+
+                {
+                    play.PlaySync();
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
