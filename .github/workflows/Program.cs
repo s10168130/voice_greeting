03e92954e7f00a735c1ca6468c26931bac026140
s10168130 <105270335+s10168130@users.noreply.github.com>
2025-04-04 07:43:32 +0000

    Add files via upload
    this includes everything about voice functions

---
+++ .github/workflows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voice_greeting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new welcome_massage() { };
+        }
+    }
+}
